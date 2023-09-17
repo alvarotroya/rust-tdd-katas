ac70d0f3453ae647c7a81910270f3525ac8a3515
Alvaro Troya <alvaro.troya.ec@googlemail.com>
2023-09-17 23:35:02 +0200

    Req 4: Implement Greeter for Vec<&str>

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 // Greeting Kata to test Jetbrain's RustRover
 // Kata instructions: https://github.com/testdouble/contributing-tests/wiki/Greeting-Kata
-
 trait Greeter {
     fn greet(&self) -> String;
 }
@@ -15,6 +14,20 @@ impl Greeter for Option<&str> {
                 }
                 return format!("Hello, {name}");
             },
+        }
+    }
+}
+
+impl Greeter for Vec<&str> {
+    fn greet(&self) -> String {
+        if self.len() == 0 {
+            greet(None)
+        } else if self.len() == 1 {
+            greet(Some(self[0]))
+        } else if self.len() == 2 {
+            format!("Hello, {} and {}", self[0], self[1])
+        } else {
+            panic!();
         }
     }
 }
@@ -46,6 +59,22 @@ mod test {
         assert_eq!(greet(Some("JERRY")), "HELLO JERRY!");
     }
 
+    // req 4
+    #[test]
+    fn test_greet_multiple_names() {
+        assert_eq!(greet(vec!["Jill", "Jane"]), "Hello, Jill and Jane");
+    }
+
+    #[test]
+    fn test_greet_empty_name_vec() {
+        assert_eq!(greet(vec![]), "Hello, my friend");
+    }
+
+    #[test]
+    fn test_greet_single_name_vec() {
+        assert_eq!(greet(vec!["Jill"]), "Hello, Jill");
+    }
 }
 
-fn main() {}
+fn main() {
+}
