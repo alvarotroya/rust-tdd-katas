bf01d509b759fa450265d4ebec0d6104ed196b77
Alvaro Troya <alvaro.troya.ec@googlemail.com>
2023-09-24 17:24:17 +0200

    Req 6: Support mixing of shouts and greets in Greeter<Vec> impl.

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ impl Greeter for Option<&str> {
         match self {
             None => String::from("Hello, my friend"),
             Some(name) => {
-                if name.to_uppercase().as_str() == *name {
+                if is_uppercase(*name) {
                     return format!("HELLO {name}!");
                 }
                 return format!("Hello, {name}");
@@ -18,19 +18,61 @@ impl Greeter for Option<&str> {
     }
 }
 
+fn is_uppercase(name :&str) -> bool {
+    name.to_uppercase().as_str() == name
+}
+
 impl Greeter for Vec<&str> {
     fn greet(&self) -> String {
         if self.len() == 0 {
-            greet(None)
-        } else if self.len() == 1 {
-            greet(Some(self[0]))
-        } else if self.len() == 2 {
-            format!("Hello, {} and {}", self[0], self[1])
-        } else {
-            let names_as_str = &self[..self.len() - 1].join(", ");
-            format!("Hello, {}, and {}", names_as_str, self[self.len() - 1])
+            return greet(None);
         }
+
+        let mut shout: Vec<&str> = Vec::new();
+        let mut greet: Vec<&str> = Vec::new();
+        for &name in self.iter(){
+            if is_uppercase(name){
+                shout.push(name)
+            } else {
+                greet.push(name)
+            }
+
+        }
+        let greet_str = _greet(&greet, false);
+        let shout_str = _greet(&shout, true);
+
+        let mut res = String::from("");
+
+        if greet.len() == 0 {
+            return shout_str;
+        } else {
+            res.push_str(&greet_str);
+        }
+        if shout.len() > 0 {
+            res.push_str(&format!(". AND {shout_str}"));
+        }
+        res
     }
+}
+
+fn _greet(names: &Vec<&str> , shout: bool) -> String {
+    let mut res : String;
+
+    if names.len() == 0 {
+        res = greet(None);
+    } else if names.len() == 1 {
+        res = greet(Some(names[0]));
+    } else if names.len() == 2 {
+        res = format!("Hello, {} and {}", names[0], names[1]);
+    } else {
+        let names_as_str = &names[..names.len() - 1].join(", ");
+        res = format!("Hello, {}, and {}", names_as_str, names[names.len() - 1]);
+    }
+
+    if shout{
+        res = res.to_uppercase();
+    }
+    res
 }
 
 fn greet<T>(arg: T) -> String where T : Greeter {
@@ -81,7 +123,12 @@ mod test {
     fn test_greet_multiple_names() {
         assert_eq!(greet(vec!["Jill", "Jane", "John"]), "Hello, Jill, Jane, and John");
     }
+
+    // req 6
+    #[test]
+    fn test_greet_and_shout() {
+        assert_eq!(greet(vec!["Jill", "BRIAN", "John"]), "Hello, Jill and John. AND HELLO BRIAN!");
+    }
 }
 
-fn main() {
-}
+fn main() {}
